b1b4e9fd7b6a92e6a68f477626a9f72a0cd69253
Ankit Tandon <Ankit.me7@gmail.com>
2024-12-28 17:42:52 +0530

    clean

--- backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 4000;
-  await app.listen(port);
-}
-bootstrap();
